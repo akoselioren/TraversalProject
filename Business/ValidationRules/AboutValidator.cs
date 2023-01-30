3b91df2f3de29e195a070bd4fe9e483ea5301285
Abdullah Köseliören <abdullahoren1@gmail.com>
2023-01-30 19:50:13 +0300

    Business Layer ve UI Layer
    Business katmanına Interface ve Classlar eklendi,
    Fulient validasyonlar eklendi,  UI katmanına geçildi ve Default Index Sayfası Partial Async'e uyarlandı.

---
+++ Business/ValidationRules/AboutValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x=> x.Description).NotEmpty().WithMessage("Açıklama kısmını boş geçemezsiniz.!");
+            RuleFor(x=> x.Description).MinimumLength(15).WithMessage("Açıklama kısmı en az 15 karakter olabilir.!");
+           
+        }
+    }
+}
---
+++ TraversalProject/ViewComponents/Default/_PopularDestinationsPartial.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalProject.ViewComponents.Default
+{
+    public class _PopularDestinationsPartial:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
