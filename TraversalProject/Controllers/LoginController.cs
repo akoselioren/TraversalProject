1acaa4394a94a65ce87a2980c81d1e3839943cc3
Abdullah Köseliören <abdullahoren1@gmail.com>
2023-02-01 21:46:20 +0300

    SignIn, SignUp ve Identity işlemleri
    Projeye SignIn, SignUp sayfaları eklendi,
    Identity eklendi, Validation'lar kullanıldı, Custom Identity Validator ile doğrulamalar oluşturuldu.

---
+++ TraversalProject/Controllers/LoginController.cs
@@ -0,0 +1,85 @@
+﻿using Entity.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TraversalProject.Models;
+
+namespace TraversalProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel p)
+        {
+            AppUser appUser = new AppUser()
+            {
+                NameSurname = p.NameSurname,
+                UserName = p.UserName,
+                Email = p.Mail
+                // PhoneNumber= p.PhoneNumber
+            };
+            if (p.Password == p.ComfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser, p.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
+        }
+        [HttpGet]
+        public IActionResult SignIn ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserRegisterViewModel p)
+        {
+            //AppUser appUser = new AppUser()
+            //{
+            //    NameSurname = p.NameSurname,
+            //    UserName = p.UserName,
+            //    Email = p.Mail
+            //    PhoneNumber= p.PhoneNumber
+            //};
+            //if (p.Password==p.ComfirmPassword)
+            //{
+            //    var result = await _userManager.CreateAsync(appUser, p.Password);
+            //    if (result.Succeeded) 
+            //    {
+            //        return RedirectToAction("/Default/Index");
+            //    }
+            //    else 
+            //    {
+            //        foreach (var item in result.Errors)
+            //        {
+            //            ModelState.AddModelError("", item.Description);
+            //        }
+            //    }
+            //}
+            return View(p);
+        }
+        
+    }
+}
---
+++ TraversalProject/Models/UserRegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalProject.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Ad ve Soyadınızı giriniz.* ")]
+        public string NameSurname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı adınızı giriniz.* ")]
+        public string UserName { get; set; }
+
+         [Required(ErrorMessage ="Lütfen Mail adresinizi giriniz.* ")]
+        public string Mail { get; set; }
+
+        //[Required(ErrorMessage = "Lütfen Telefon numaranızı giriniz. ")]
+        //public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz.* ")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi tekrar giriniz.*")]
+        [Compare("Password",ErrorMessage ="*-- Şifreler birbiriyle uyuşmuyor tekrar giriniz.! --*")]
+        public string ComfirmPassword { get; set; }
+    }
+}
